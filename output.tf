81d45dafa6928ab186d3dd721ba8302333f38b1a
Slav Donchev <slav.donchev@gmail.com>
2019-10-23 15:35:15 +0300

    cleanup comented code

--- output.tf
+++ output.tf
@@ -1,9 +1,4 @@
 output "resource_group" {
     description = "Properties of the created Azure resource group."
-    # value = {
-    #     name = azurerm_resource_group.rg.name
-    #     id = azurerm_resource_group.rg.id
-    #     location = azurerm_resource_group.rg.location
-    # }
     value = azurerm_resource_group.rg
 }
