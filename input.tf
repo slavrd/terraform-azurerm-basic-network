5e1facf9ad778f8ad70893db70a4003aa74a17d6
Slav Donchev <slav.donchev@gmail.com>
2019-10-23 14:21:57 +0300

    add resource group creation

---
+++ input.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+    type = string
+    description = "Name of the resource group to be created."
+}
+
+variable "location" {
+    type = string
+    description = "The Azure location in which to create the resources."
+}
