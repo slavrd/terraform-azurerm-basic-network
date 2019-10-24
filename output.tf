099a2c4737212aced6cd8095d1f099bbc8c3d841
Slav Donchev <slav.donchev@gmail.com>
2019-10-24 11:16:20 +0300

    add vnet and subnets to tf code

--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "resource_group" {
     description = "Properties of the created Azure resource group."
     value = azurerm_resource_group.rg
 }
+
+output "vnet" {
+    description = "Properties of the created Azure virtual network."
+    value = azurerm_virtual_network.vnet
+}
+
+output "subnets" {
+    description = "Properties of the created Azure subnets."
+    value = values(azurerm_subnet.subnets)
+}
