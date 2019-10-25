9b0a23c3633a5ce58fbc3cff2eae34b9e604541b
Slav Donchev <slav.donchev@gmail.com>
2019-10-25 14:18:26 +0300

    update tests to check for tags

--- test/integration/default/controls/outputs.rb
+++ test/integration/default/controls/outputs.rb
@@ -3,16 +3,37 @@ control 'outputs' do
     title 'Outputs test'
     desc 'Simple tests that the outputs are not empty'
 
-    describe input('rg')['name'] do 
-        it{should match /.+/ }
+    describe 'rg output' do
+        subject{ input('rg') }
+
+        it 'should not be empty' do
+            expect(subject['name']).to(match /.+/)
+        end
+
+        it 'tags should not be empty' do
+            expect(subject['tags'].empty?).to(be false)
+        end
+        
     end
 
-    describe input('vnet') do
-        its('length') {should eq 1}
+    describe 'vnet output' do
+        subject{ input('vnet') }
+
+        it 'array should have 1 element' do
+            expect(subject.length).to(eq 1)
+        end
+
+        it 'tags should not be empty' do
+            expect(subject[0]['tags'].empty?).to(be false)
+        end
+
     end
 
-    describe input('subnets') do
-        its('length') {should be > 0}
+    describe 'subnets output' do
+        subject{ input('subnets').length }
+        it 'array should not be empty' do
+            expect(subject).to(be > 0)
+        end
     end
 
 end
