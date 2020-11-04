7a581722d399df3d61f7b082bbd1f98eb72b09ba
Slav Donchev <slav.donchev@gmail.com>
2020-11-04 15:13:10 +0200

    fix spelling

--- input.tf
+++ input.tf
@@ -28,11 +28,11 @@ variable "vnet_name" {
 
 variable "vnet_cidrs" {
   type        = list(string)
-  description = "List of CIDR ranges for the Azure virtual network address spaces."
+  description = "List of CIDRs for the Azure virtual network address spaces."
 }
 
 variable "vnet_subnet_cidrs" {
   type        = list(string)
-  description = "List of CIDR ranges for subnets creation. It's meaningless to set this if vnet_address_space is not set also. It's the user's responsibility to ensure that the subnets are calculated correctly."
+  description = "List of CIDRs for subnets creation. It's meaningless to set this if vnet_address_space is not set also. It's the user's responsibility to ensure that the subnets are calculated correctly."
   default     = []
 }
