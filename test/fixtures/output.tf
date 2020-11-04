49842aecde6a11c920af5e02103935fca97c6ba6
Slav Donchev <slav.donchev@gmail.com>
2020-11-04 09:59:25 +0200

    fix formatting

--- test/fixtures/output.tf
+++ test/fixtures/output.tf
@@ -1,14 +1,14 @@
 output "rg" {
-    description = "Properties of the created Azure resource group."
-    value = module.testmodule.rg
+  description = "Properties of the created Azure resource group."
+  value       = module.testmodule.rg
 }
 
 output "vnet" {
-    description = "Properties of the created Azure virtual network."
-    value = module.testmodule.vnet
+  description = "Properties of the created Azure virtual network."
+  value       = module.testmodule.vnet
 }
 
 output "subnets" {
-    description = "Properties of the created Azure subnets."
-    value = module.testmodule.subnets
+  description = "Properties of the created Azure subnets."
+  value       = module.testmodule.subnets
 }
